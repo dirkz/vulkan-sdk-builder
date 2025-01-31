07c9819506d968cdb19c6fbd71c90d4734dacd3f
Dirk Zimmermann <me@dirkz.com>
2025-01-31 11:05:21 +0100

    Can build the loader

--- build.ps1
+++ build.ps1
@@ -1,10 +1,20 @@
 $version = "vulkan-sdk-1.4.304"
 $build_dir = "$PSScriptRoot\build"
 $prefix = "$PSScriptRoot\install"
+$config = "Debug"
 
 Write-Host "Building $version in $build_dir"
 
 $project = "Vulkan-Headers"
 $build_dir_project = "$build_dir\$project"
-cmake -B $build_dir_project -S $project
-cmake --install $build_dir_project --prefix $prefix
+& cmake -B $build_dir_project -S $project
+& cmake --install $build_dir_project --prefix $prefix
+
+$VULKAN_HEADERS_INSTALL_DIR = $prefix
+
+$project = "Vulkan-Loader"
+$build_dir_project = "$build_dir\$project"
+& cmake -B $build_dir_project -S $project `
+    -D VULKAN_HEADERS_INSTALL_DIR=$VULKAN_HEADERS_INSTALL_DIR
+& cmake --build $build_dir_project --config $config
+& cmake --install $build_dir_project --prefix $prefix --config $config
