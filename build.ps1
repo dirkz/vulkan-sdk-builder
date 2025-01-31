8ea04a85d5a817cb600017b911e6467b716d6c5b
Dirk Zimmermann <me@dirkz.com>
2025-01-31 12:09:31 +0100

    Use build function

--- build.ps1
+++ build.ps1
@@ -2,42 +2,44 @@ Param(
    [switch]$Clean
 )
 
-$version = "vulkan-sdk-1.4.304"
+#$version = "vulkan-sdk-1.4.304"
+#$nl = [Environment]::NewLine
+
 $build_dir = "$PSScriptRoot\build"
 $prefix = "$PSScriptRoot\install"
 $config = "Debug"
-$nl = [Environment]::NewLine
 
 if ($Clean)
 {
     Remove-Item -LiteralPath $build_dir -Force -Recurse
 }
 
-Write-Host "Building $version in $build_dir"
+$Definitions = @{VULKAN_HEADERS_INSTALL_DIR = $prefix}
 
-$project = "Vulkan-Headers"
-$build_dir_project = "$build_dir\$project"
-Write-Host "$nl$project"
-Write-Host "======================="
-& cmake -B $build_dir_project -S $project
-& cmake --install $build_dir_project --prefix $prefix
+function Build {
+    Param(
+        [string]$ProjectName,
+        [string[]]$Defines
+    )
 
-$VULKAN_HEADERS_INSTALL_DIR = $prefix
+    $defs = ""
 
-$project = "Vulkan-Loader"
-$build_dir_project = "$build_dir\$project"
-Write-Host "$nl$project"
-Write-Host "======================="
-& cmake -B $build_dir_project -S $project `
-    -D VULKAN_HEADERS_INSTALL_DIR=$VULKAN_HEADERS_INSTALL_DIR
-& cmake --build $build_dir_project --config $config
-& cmake --install $build_dir_project --prefix $prefix --config $config
+    foreach ($def in $Defines) {
+        $definition = $Definitions[$def]
+        $defs += "-D $def=$definition"
+    }
 
-$project = "Vulkan-Utility-Libraries"
-$build_dir_project = "$build_dir\$project"
-Write-Host "$nl$project"
-Write-Host "======================="
-& cmake -B $build_dir_project -S $project `
-    -D VULKAN_HEADERS_INSTALL_DIR=$VULKAN_HEADERS_INSTALL_DIR
-& cmake --build $build_dir_project --config $config
-& cmake --install $build_dir_project --prefix $prefix --config $config
+    Write-Host ""
+    Write-Host "$ProjectName"
+    Write-Host "======================="
+    Write-Host ""
+
+    $build_dir_project = "$build_dir\$ProjectName"
+    & cmake -B $build_dir_project -G Ninja -S $ProjectName $defs
+    & cmake --build $build_dir_project --config $config
+    & cmake --install $build_dir_project --prefix $prefix --config $config
+}
+
+Build -ProjectName "Vulkan-Headers"
+Build -ProjectName "Vulkan-Loader" -Defines @("VULKAN_HEADERS_INSTALL_DIR")
+Build -ProjectName "Vulkan-Utility-Libraries" -Defines @("VULKAN_HEADERS_INSTALL_DIR")
